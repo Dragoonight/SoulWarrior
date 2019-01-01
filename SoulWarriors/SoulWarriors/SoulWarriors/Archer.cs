e2c8a0ed5ff046e8f1cc71a2316a79e9291c4e71
Unknown <drakmusse.musse123@gmail.com>
2019-01-01 23:44:46 +0100

    Added control methods and Keyboard
    Added methods in Archer and Knight
    Implemented static keyboard variabel

--- SoulWarriors/SoulWarriors/SoulWarriors/Archer.cs
+++ SoulWarriors/SoulWarriors/SoulWarriors/Archer.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace SoulWarriors
 {
     class Archer : Player
     {
+        private void GetInput()
+        {
+            if (currentKBState.IsKeyDown(Keys.W))
+            {
+
+            }
+
+            if (currentKBState.IsKeyDown(Keys.A))
+            {
+
+            }
+
+            if (currentKBState.IsKeyDown(Keys.D))
+            {
+
+            }
+
+            if (currentKBState.IsKeyDown(Keys.S))
+            {
+
+            }
+           
+        }
     }
 }
