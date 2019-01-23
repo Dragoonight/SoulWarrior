7d61d0c872a70faf3fc8c7a12f3c2c364380318b
Dragoo <drakmusse.musse123@gmail.com>
2019-01-23 21:13:14 +0100

    Added basic functions and variables

--- SoulWarriors/SoulWarriors/SoulWarriors/Enemy.cs
+++ SoulWarriors/SoulWarriors/SoulWarriors/Enemy.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace SoulWarriors
 {
     class Enemy
     {
-
+        //Variables
+        private Texture2D enemyTestTexture;
+        private int speed;
+        private Vector2 enemyPosition;
+        private Rectangle rectTarget;
+        private Rectangle rectSpawn;
 
 
         // Key Rectangles/Areas (Spawn and target)
 
 
-        
+
         // Go directly for the door
 
 
@@ -26,14 +38,25 @@ namespace SoulWarriors
 
 
         // LoadContent (temporary)
-
+        public void LoadContent(ContentManager content)
+        {
+            enemyTestTexture = content.Load<Texture2D>("Insert name here");
+        }
 
         // Update
+        public void Update(GameTime gameTime)
+        {
 
+        }
 
 
         // Draw
-
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw();
+            spriteBatch.End();
+        }
 
 
     }
