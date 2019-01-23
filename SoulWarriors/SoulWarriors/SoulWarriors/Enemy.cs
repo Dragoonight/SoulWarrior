b6ccac2c94873494117e8532dd71230f17fb2c04
Unknown <julius.roolf@gmail.com>
2019-01-23 20:53:22 +0100

    Added template

--- SoulWarriors/SoulWarriors/SoulWarriors/Enemy.cs
+++ SoulWarriors/SoulWarriors/SoulWarriors/Enemy.cs
@@ -7,5 +7,34 @@ namespace SoulWarriors
 {
     class Enemy
     {
+
+
+
+        // Key Rectangles/Areas (Spawn and target)
+
+
+        
+        // Go directly for the door
+
+
+
+        // Mainly attack player (check who is closed to the enemy)
+
+
+
+        // Go for door, attack player if near by (Check which circle is colliding first)
+
+
+        // LoadContent (temporary)
+
+
+        // Update
+
+
+
+        // Draw
+
+
+
     }
 }
